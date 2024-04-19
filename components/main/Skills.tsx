daf1b2a023a8e1364a6bc8b536893a3b36da36e3
Tianhao Zhang <zhangth1990@hotmail.com>
2024-04-19 15:27:28 -0600

    add skill data

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -12,6 +12,17 @@ function Skills() {
         >
             <SkillText />
             <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+                {Skill_data.map((image, index) => (
+                    <SkillDataProvider
+                        key={index}
+                        src={image.Image}
+                        width={image.width}
+                        height={image.height}
+                        index={index}
+                    />
+                ))}
+            </div>
+            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
                 {Frontend_skill.map((image, index) => (
                     <SkillDataProvider
                         key={index}
